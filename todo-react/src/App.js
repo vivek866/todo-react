2ba0e87ab28e9482a1573ce56a5aa057304e1ef1
Roshan Suganthan <suganros52@gmail.com>
2022-10-14 00:02:57 +0530

    react-todo

---
+++ todo-react/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      vanakam da maple
+      </header>
+    </div>
+  );
+}
+
+export default App;
