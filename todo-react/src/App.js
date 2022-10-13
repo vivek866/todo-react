2d92caa6330ffb4046c6471ed15e33bd9a5a4cf0
Roshan Suganthan <suganros52@gmail.com>
2022-10-14 00:07:28 +0530

    Vadivelu-Comp

--- todo-react/src/App.js
+++ todo-react/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Vadivelu from './Component/Vadivelu';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       vanakam da maple
+      <Vadivelu/>
       </header>
     </div>
   );
