36cce75a8c3d31d22734b15e33a57ce4a2a49ae6
Roshan Suganthan <suganros52@gmail.com>
2022-10-14 00:14:53 +0530

    santhanam-comp

---
+++ todo-react/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Santhanam from './Component/Santhanam';
+import Vadivelu from './Component/Vadivelu';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      vanakam da maple
+      <Vadivelu/>
+      <Santhanam />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-react/src/Component/Santhanam.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Santhanam() {
+  return (
+    <div>Santhanam</div>
+  )
+}
