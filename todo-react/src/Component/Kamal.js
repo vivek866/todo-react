f7277600c2985132be8c3749ae1247300c8dca1c
KamalRenu <91111005+KamalRenu@users.noreply.github.com>
2022-10-25 18:56:50 +0530

    Update Kamal.js

--- todo-react/src/Component/Kamal.js
+++ todo-react/src/Component/Kamal.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Kamal(){
   return(
     <h1>Hello Kamal</h1>
-    <a href="https://google.com">Google</a>
+    <a href="https://google.com">Google!!!</a>
   )
 }
 
